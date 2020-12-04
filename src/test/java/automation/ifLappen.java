1f9d66e8feebf6e8c117839bd3f4ccd61e4db705
SemihTut <yalaantv@gmail.com>
2020-12-04 23:00:38 +0200

    appium

--- src/test/java/automation/ifLappen.java
+++ src/test/java/automation/ifLappen.java
@@ -8,10 +8,15 @@ import java.net.URL;
 import java.util.function.Function;
 import java.net.MalformedURLException;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
@@ -19,8 +24,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
     public class ifLappen {
 
-        public static void main(String[] args) throws MalformedURLException, InterruptedException {
-
+        private AppiumDriver<MobileElement> driver;
+        @Test
+        public void test1() throws MalformedURLException, InterruptedException {
             DesiredCapabilities caps = new DesiredCapabilities();
 
             // Set your access credentials
@@ -28,21 +34,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
             caps.setCapability("browserstack.key", "wf3fBrZGCzB5Vsnp7TWo");
 
             // Set URL of the application under test
-            caps.setCapability("app", "bs://027637d2ffe5c2aca1c4fe77c08572e0929b17c4");
+            caps.setCapability("app", "bs://9e6671f026c226f00c00b4a1bb8c5fdac66fa850");
 
             // Specify device and os_version for testing
-            caps.setCapability("device", "Samsung Galaxy Tab S7");
-            caps.setCapability("os_version", "10.0");
+            caps.setCapability("device", "Google Pixel 2");
+            caps.setCapability("os_version", "8.0");
 
             // Set other BrowserStack capabilities
-            caps.setCapability("project", "First Java Project");
+            caps.setCapability("project", "IF Lappen Project");
             caps.setCapability("build", "Java Android");
-            caps.setCapability("name", "first_test");
+            caps.setCapability("name", "Installing Test");
 
 
             // Initialise the remote Webdriver using BrowserStack remote URL
             // and desired capabilities defined above
-            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+            driver = new AppiumDriver<MobileElement>(
                     new URL("http://hub.browserstack.com/wd/hub"), caps);
 
 
@@ -55,4 +61,3 @@ import org.openqa.selenium.remote.DesiredCapabilities;
         }
 
     }
-
